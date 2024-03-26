78e041c7898c8f1738dce6b6ef2d40787582c212
Mitchellnkem <Mitchellnkemgmailcomgit config --global user.email Mitchellnkemgmail.com>
2024-03-26 12:21:00 +0000

    Subsequnet commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-
-
 import Navbar from './components/Navbar/Navbar';
+import {Routes, Route} from "react-router-dom";
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Home from './pages/Home/Home';
+
+
+
 
 const App = () => {
   
@@ -10,6 +15,13 @@ const App = () => {
   return (
       <div className="app">
         <Navbar />
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+        </Routes>
       </div>
       
   );
